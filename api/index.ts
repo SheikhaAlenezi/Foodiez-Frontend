664e7a08d990055adab0329e80a1622aabbd44d5
SheikhaAlenezi <shei5ha98@gmail.com>
2025-09-13 18:12:29 +0300

    update for lil shaikha

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,10 @@ import { getToken } from "./storage";
 const instance = axios.create({
   baseURL: "http://192.168.8.107:8000/api",
 });
-
+// little-shaikha "http://:8000/api"
+//
+// sheikha "http://192.168.8.107:8000/api"
+//
 instance.interceptors.request.use(async (config) => {
   const token = await getToken();
   if (token) {
